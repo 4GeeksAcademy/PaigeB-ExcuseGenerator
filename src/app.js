39dbdcc754c717dd3563f5c1e97c6a512d2ce59e
Techprenuer1 <125614142+Techprenuer1@users.noreply.github.com>
2023-05-17 23:58:45 +0000

    Need to fix

--- src/app.js
+++ src/app.js
@@ -4,8 +4,24 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { formatWithCursor } from "prettier";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = domain();
+  });
+  domain = () => {
+    let first = ["the", "her", "him", "prime", "beauty", "dance"];
+    let second = ["great", "big", "saucy", "blue", "happy", "domain"];
+    let third = [".com", ".org", ".net", ".edu", ".io", ".info"];
+
+    for (let i = 0; i < first.length; i++) {
+      for (let a = 0; a < second.length; a++) {
+        for (let b = 0; b < third.length; b++) {
+          console.log(first[i] + second[a] + third[b]);
+        }
+      }
+    }
+    return first[i] + " " + second[a] + " " + third[b];
+  };
 };
